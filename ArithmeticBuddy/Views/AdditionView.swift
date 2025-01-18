10482cc7d7c84357fbbfc55f7664188f20d0929a
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-18 10:45:28 -0500

    Created the project and finished initial project organization.

---
+++ ArithmeticBuddy/Views/AdditionView.swift
@@ -0,0 +1,18 @@
+//
+//  AdditionView.swift
+//  ArithmeticBuddy
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-18.
+//
+
+import SwiftUI
+
+struct AdditionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AdditionView()
+}
