3da0309932fc67a27fe0033e6ec3cc0ffdc48f7e
Kaan Caliskan <kcaliskan@lcs.on.ca>
2025-01-18 10:40:35 -0500

    Initial Commit

---
+++ ArithmeticBuddy/ArithmeticBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArithmeticBuddyApp.swift
+//  ArithmeticBuddy
+//
+//  Created by Ahmet Kaan Caliskan on 2025-01-18.
+//
+
+import SwiftUI
+
+@main
+struct ArithmeticBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
